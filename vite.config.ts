9bb6f891cce290e725efdc6d5d2a26e2634195df
sangyong1203 <sangyong1203@trgtech.co.kr>
2025-07-17 15:44:42 +0900

    deploy to github 설정

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ import Components from 'unplugin-vue-components/vite'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode })=>{
   return {
-
+    base: '/apidoc/',
     // base:'https://example.com',
     plugins: [
       vue(),
